16c3af6736e86ec7e287c4ea3edda322136b1d74
Aditya Mayukh Som <adi.kg2@gmail.com>
2025-01-21 02:54:41 +0530

    Added dotenv support

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import uvicorn, os
+from dotenv import load_dotenv
 from summarization import SummarizationApplication
 
 if __name__ == "__main__":
+    load_dotenv(verbose=True, override=True)
     app = SummarizationApplication.generate()
     port = int(os.environ.get("PORT", 8080))
     uvicorn.run(app, host="0.0.0.0", port=port)
