31ce20fa28dd0045901812931cc83941d6c91c91
Aditya Mayukh Som <adi.kg2@gmail.com>
2025-01-21 02:24:41 +0530

    Added model task in API

--- summarization/models.py
+++ summarization/models.py
@@ -5,6 +5,7 @@ class SummarizationRequest(APIModel):
     paper_content: str
     preferred_model: str
     maximum_tokens: int
+    model_task: str
 
 
 class SummarizationResponse(APIModel):
@@ -12,9 +13,10 @@ class SummarizationResponse(APIModel):
 
 
 class AvailableModel:
-    def __init__(self, hf_model_id: str, display_name: str):
+    def __init__(self, hf_model_id: str, display_name: str, task: str):
         self.hf_model_id = hf_model_id
         self.display_name = display_name
+        self.task = task
 
     def __hash__(self):
-        return hash((self.hf_model_id, self.display_name))
+        return hash(self.hf_model_id)
