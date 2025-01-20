232b944d623e6aabc50857202b24534ac5643298
Aditya Mayukh Som <adi.kg2@gmail.com>
2025-01-21 01:08:47 +0530

    Refactored application file

--- summarization/application.py
+++ summarization/application.py
@@ -1,37 +1,33 @@
-import fastapi
-import fastapi.staticfiles
-import fastapi.middleware.cors
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 
-from .routes import api_router
+from .routes import apis_router, view_router
 
 
 class SummarizationApplication:
-    # we are having this just for development purposes, in production,
-    # the vue application is served by the python backend itself, so
+    # The frontend is served by the python backend itself, so
     # allowed origins can be an empty array without allowing localhost
-    __allowed_origins = ["http://localhost:5173"]
+    __allowed_origins = []
     __allowed_headers: list[str] = ["*"]
     __allowed_methods: list[str] = ["*"]
     __allowed_credentials: bool = True
 
     @classmethod
-    def generate(cls):
-        app = fastapi.FastAPI()
+    def generate(cls, static_dir="public"):
+        app = FastAPI()
+        static_app = StaticFiles(directory=static_dir)
 
         app.add_middleware(
-            fastapi.middleware.cors.CORSMiddleware,
+            CORSMiddleware,
             allow_origins=cls.__allowed_origins,
             allow_methods=cls.__allowed_methods,
             allow_headers=cls.__allowed_headers,
             allow_credentials=cls.__allowed_credentials,
         )
 
-        app.include_router(api_router)
-
-        app.mount(
-            path="/",
-            app=fastapi.staticfiles.StaticFiles(directory="dist/", html=True),
-            name="static",
-        )
+        app.mount(path="/public", app=static_app, name="public")
+        app.include_router(apis_router)
+        app.include_router(view_router)
 
         return app
